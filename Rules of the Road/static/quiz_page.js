e401419fbd5275abc7d1bb4cec30e7fc64c1ea0b
keithpcruz <103606282+keithpcruz@users.noreply.github.com>
2022-04-18 15:34:35 -0400

    Updated quiz_page.html and .js

--- Rules of the Road/static/quiz_page.js
+++ Rules of the Road/static/quiz_page.js
@@ -1,5 +1,55 @@
-//let questions = {{questions|tojson}} 
 
 $(document).ready(function(){
-	//
+	let url = window.location.href
+	let currentIdx = url.slice(-1)
+	console.log("current id = " + currentIdx)
+
+	if(currentIdx >=2){
+		$("#testPrevious").show()
+	}
+
+	else{
+		$("#testPrevious").hide()
+	}
+
+	if(currentIdx <= 5){
+		$("#testNext").show()
+	}
+
+	else{
+		$("#testNext").hide()
+	}
+
+
+	$("#testPrevious").click(function(){
+		console.log("clicked previous")
+
+		if($("#testPrevious").show()){
+			let prevIdx = parseInt(currentIdx) - 1
+			let url = "/learn/" + prevIdx
+            location.href = url
+			console.log("updated: " + prevIdx)
+		}
+
+	})
+
+	$("#testNext").click(function(){
+		console.log("clicked next")
+
+		if($("#testNext").show()){
+			if(currentIdx == 5){
+				let url = "/quiz"
+				location.href = url
+			}
+			else{
+				let nextIdx = parseInt(currentIdx) + 1
+				let url = "/learn/" + nextIdx
+				location.href = url
+			}
+
+			console.log("updated: " + nextIdx)
+
+		}
+
+	})
 })
